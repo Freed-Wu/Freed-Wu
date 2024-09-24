250d041754af69c09c9d54ef618ac56b4ca54260
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-09-24 21:53:50 +0800

    :bug: Fix fugit2.nvim in NixOS
    Update shellcheck's source directive
    perl-languageserver supports perl-tidy
    bash-language-server supports shfmt
    Update smartinput config for jq
    Add jq -rj due to https://github.com/01mf02/jaq/issues/217
    Update jq ftplugin
    Add tmux-rime
    Add rl_custom_rime
    Update tmux config
    Fix C/C++ templates
    Add templates for neovim plugins

--- .config/nvim/lua/plugins/fugit2.lua
+++ .config/nvim/lua/plugins/fugit2.lua
@@ -1,2 +1,18 @@
+-- luacheck: ignore 111 113
+---@diagnostic disable: undefined-global
 -- https://github.com/SuperBo/fugit2.nvim/issues/97
-require('fugit2').setup({})
+---@diagnostic disable: lowercase-global
+setting = {}
+local ext = "so"
+if vim.fn.has("osx") == 1 then
+    ext = "dynlib"
+elseif vim.fn.has("win32") == 1 then
+    ext = "dll"
+end
+if vim.fn.filereadable("/run/current-system/nixos-version") == 1 then
+    loadstring("setting.libgit2_path = " ..
+        io.popen(vim.fs.joinpath(os.getenv("HOME"), "script", "get-libgit2_path.nix")):read() ..
+        " .. '" .. ext .. "'")()
+end
+require('fugit2').setup(setting)
+setting = nil
