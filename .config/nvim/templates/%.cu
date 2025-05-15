52a1281c5aa62d87198036809c4d173c7ed8dbdb
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-05-15 16:07:15 +0800

    :adhesive_bandage: Update github actions
    Upgrade to plasma 6
    Disable nano in NixOS config
    Replace xsel by wayclip
    
    Fix wgetrc for wgetpaste
    Update pacman config
    
    Cache tmux-status-bar's result
    
    Create shell.nix for building tree-sitter parsers
    Remove old code from nvim config
    Add screenkey.nvim
    Add hotkey '<<<'
    Add ftplugins for lex, yacc, nasm
    Pin tree sitter parsers' version for using binary rocks
    
    Disable fzf-tab's preview on Windows
    Disable coc-git, committa.vim on Windows
    Fix shellcmdflag on Windows
    Add AppData/Local/.xmake
    Add AppData/Local/Microsoft/WindowsApps/ to $PATH
    Add PYTHONUTF8 for windows

--- .config/nvim/templates/%.cu
+++ .config/nvim/templates/%.cu
@@ -1,11 +1,16 @@
 #if 0
-bin="$(basename "$0")" && bin="${bin%%.*}" && nvcc "$0" -o "$bin" && exec ./"$bin" "$@"
+bin="$(basename "$0")" && bin="${bin%%.*}" && nvcc -g Wall -o "$bin" "$0" && exec ./"$bin" "$@" || exit $?
 #endif
 #include <err.h>
 #include <cerrno>
 #include <cstdio>
 #include <cstdlib>
-int main(int argc, char *argv[]) {
+
+__global__ void kernel() {
   {% here %}
+}
+
+int main(int argc, char *argv[]) {
+  kernel<<<1, 1>>>();
   return EXIT_SUCCESS;
 }
