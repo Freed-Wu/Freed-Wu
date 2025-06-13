1152a744ae0981cc4c23bae8583ab16e817639c3
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-06-13 13:33:46 +0800

    :truck: Rename main to __main to avoid conflict
    Update Windows terminal config
    Update proxychains config
    Add hosts config

--- .config/pudb/pudb_stringifier.py
+++ .config/pudb/pudb_stringifier.py
@@ -2,9 +2,13 @@ r"""Pudb Stringifier
 ====================
 """
 
+import logging
+import os
 from typing import Any
 
 from pudb.var_view import default_stringifier
+
+logger = logging.getLogger(__name__)
 
 
 def pudb_stringifier(obj: Any) -> Any:
@@ -14,9 +18,16 @@ def pudb_stringifier(obj: Any) -> Any:
     :type obj: Any
     :rtype: Any
     """
+    pythonstartup = os.getenv("PYTHONSTARTUP", os.devnull)
     try:
-        from python.describe import describe
+        with open(pythonstartup) as f:
+            code = f.read()
+        exec(code)  # nosec: B102
+    except Exception as e:
+        logger.warning("skip $PYTHONSTARTUP due to " + " ".join(e.args))
+    try:
+        from __main.describe import describe
 
         return describe(obj)
-    except ImportError:
+    except ModuleNotFoundError:
         return default_stringifier(obj)
