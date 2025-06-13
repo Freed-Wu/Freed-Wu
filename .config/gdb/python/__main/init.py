ce5e7fb0f4bcdb04940707dece356ea1e66d8261
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-06-13 13:33:46 +0800

    :truck: Rename main to __main to avoid conflict
    Update Windows terminal config

--- .config/gdb/python/__main/init.py
+++ .config/gdb/python/__main/init.py
@@ -5,12 +5,12 @@ Wrap ``__main__.py`` to ensure only ``my`` to be imported.
 """
 
 try:
-    from main.__main__ import my
+    from __main.__main__ import my
 except ImportError:
     import os
     import sys
 
     sys.path.insert(0, os.path.expanduser("~/.config/gdb/python"))
-    from main.__main__ import my  # noqa: F401
+    from __main.__main__ import my  # noqa: F401
 
     sys.path.pop(0)
